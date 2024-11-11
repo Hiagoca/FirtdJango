ac71398e91fe60b4836f2587ebe26f1872e5829d
Hiagoca <hiagoca.senac@gmail.com>
2024-11-11 15:47:49 -0300

    2024.11.11

--- chatsite/chat/urls.py
+++ chatsite/chat/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),  # Define a página inicial do chat
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
+    path('success/', views.success, name='success'),
 ]
